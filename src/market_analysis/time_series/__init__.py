f0665f1d3473eace5a0726d9abd47287d28b58c7
Whit Walters <whitney.walters@gmail.com>
2025-01-29 13:02:58 -0800

    Add __init__.py for time_series package

---
+++ src/market_analysis/time_series/__init__.py
@@ -0,0 +1,10 @@
+"""
+Time series analysis package for financial market data.
+
+This package provides tools for analyzing financial time series data,
+including returns calculations, statistical analysis, and technical indicators.
+"""
+
+from .returns import ReturnCalculator
+
+__all__ = ['ReturnCalculator']
